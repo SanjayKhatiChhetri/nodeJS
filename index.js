8fb122092f44677d10ada739dcb738cb813bc2b4
SanjayKhatiChhetri <sanjaykhati579@gmail.com>
2023-07-04 13:31:08 +0300

    ready for deployment

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+// Set the view engine to pug
+app.set("view engine", "pug");
+// Middleware
+app.use(helmet())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+let customers = [
+	{id: '1588323375416', firstName: 'John', lastName: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
+	{id: '1588323375417', firstName: 'Mary', lastName: 'Smith', email: 'mary@smith.com', phone: '6654113'},
+	{id: '1588323375418', firstName: 'Peter', lastName: 'North', email: 'peter@north.com', phone: '901176'},
+  ]
+
+// Routes
+app.get("/", (req, res) => {
+	res.render("customers.pug", { customers });
+});
+
+app.get("/addCustomer", (req, res) => {
+	res.render("add_customer.pug");
+});
+app.post("/addCustomer", (req, res) => {
+	const { firstName, lastName, email, phone } = req.body;
+	const id = Date.now().toString();
+	customers.push({ id, firstName, lastName, email, phone });
+	res.redirect("/");
+})
+
+app.listen(port, () => {
+	console.log(`Server is running on port http://localhost:${port}`);
+});
